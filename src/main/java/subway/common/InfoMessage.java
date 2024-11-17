ced28763b732e13597cde5eafc428dc22ac4c3ad
kgy1008 <gayeon1008@g.skku.edu>
2024-11-17 16:45:45 +0900

    feat(InfoMessage): define info messages

---
+++ src/main/java/subway/common/InfoMessage.java
@@ -0,0 +1,18 @@
+package subway.common;
+
+public enum InfoMessage {
+
+    REGISTER_STATION("지하철 역이 등록되었습니다."),
+    DELETE_STATION("지하철 역이 삭제되었습니다.");
+
+    private static final String HEADER = "[INFO] ";
+    private final String message;
+
+    InfoMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return HEADER + message;
+    }
+}
