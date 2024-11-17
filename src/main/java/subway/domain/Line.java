023f3be5ede100cbf5e99d910874c167437aae58
kgy1008 <gayeon1008@g.skku.edu>
2024-11-17 15:51:19 +0900

    feat: validate station line

--- src/main/java/subway/domain/Line.java
+++ src/main/java/subway/domain/Line.java
@@ -1,9 +1,18 @@
 package subway.domain;
 
-public class Line {
-    private String name;
+import static subway.common.ErrorMessage.CONFLICT_LINE;
+import static subway.common.ErrorMessage.INVALID_FORMAT;
+import static subway.common.ErrorMessage.INVALID_LENGTH;
 
-    public Line(String name) {
+import subway.repository.LineRepository;
+
+public class Line {
+    private static final int MIN_LENGTH = 2;
+    private static final String ESSENTIAL_SUFFIX = "호선";
+    private final String name;
+
+    public Line(final String name) {
+        validate(name);
         this.name = name;
     }
 
@@ -11,5 +20,27 @@ public class Line {
         return name;
     }
 
-    // 추가 기능 구현
+    private void validate(final String name) {
+        validateName(name);
+        validateDuplicated(name);
+        validateFormat(name);
+    }
+
+    private void validateName(final String name) {
+        if (name.length() < MIN_LENGTH) {
+            throw new IllegalArgumentException(INVALID_LENGTH.getMessage());
+        }
+    }
+
+    private void validateDuplicated(final String name) {
+        if (LineRepository.isExistLineByName(name)) {
+            throw new IllegalArgumentException(CONFLICT_LINE.getMessage());
+        }
+    }
+
+    private void validateFormat(final String name) {
+        if (!name.contains("ESSENTIAL_SUFFIX")) {
+            throw new IllegalArgumentException(INVALID_FORMAT.getMessage());
+        }
+    }
 }
---
+++ src/main/java/subway/repository/LineRepository.java
@@ -0,0 +1,32 @@
+package subway.repository;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import subway.domain.Line;
+
+public class LineRepository {
+    private static final List<Line> lines = new ArrayList<>();
+
+    public static List<Line> lines() {
+        return Collections.unmodifiableList(lines);
+    }
+
+    public static void addLine(final Line line) {
+        lines.add(line);
+    }
+
+    public static void addLines(final List<Line> subwayLines) {
+        lines.addAll(subwayLines);
+    }
+
+    public static boolean deleteLineByName(final String name) {
+        return lines.removeIf(line -> Objects.equals(line.getName(), name));
+    }
+
+    public static boolean isExistLineByName(final String name) {
+        return lines.stream().anyMatch(line -> Objects.equals(line.getName(), name));
+    }
+
+}
