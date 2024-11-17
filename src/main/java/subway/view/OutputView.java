823de3557aae4decc8e455852939e47b273e1b39
kgy1008 <gayeon1008@g.skku.edu>
2024-11-17 16:47:12 +0900

    feat(OutputView): print menu

--- src/main/java/subway/view/OutputView.java
+++ src/main/java/subway/view/OutputView.java
@@ -1,19 +1,25 @@
 package subway.view;
 
 public class OutputView {
-    private static final String MAIN_MENU = "## 메인 화면";
-    private static final String STATION_MANAGEMENT = "1. 역 관리";
-    private static final String LINE_MANAGEMENT = "2. 노선 관리";
-    private static final String SECTION_MANAGEMENT = "3. 구간 관리";
-    private static final String DISPLAY_ROUTE = "4. 지하철 노선도 출력";
-    private static final String EXIT = "Q. 종료";
+    private static final String NEW_LINE = System.lineSeparator();
+    private static final String MAIN_MENU = "## 메인 화면" + NEW_LINE
+            + "1. 역 관리" + NEW_LINE
+            + "2. 노선 관리" + NEW_LINE
+            + "3. 구간 관리" + NEW_LINE
+            + "4. 지하철 노선도 출력" + NEW_LINE
+            + "Q. 종료";
+    private static final String STATION_MENU = "## 역 관리 화면" + NEW_LINE
+            + "1. 역 등록" + NEW_LINE
+            + "2. 역 삭제" + NEW_LINE
+            + "3. 역 조회" + NEW_LINE
+            + "B. 돌아가기";
+    private static final String INFO = "[INFO] ";
 
     public void printMainMenu() {
         System.out.println(MAIN_MENU);
-        System.out.println(STATION_MANAGEMENT);
-        System.out.println(LINE_MANAGEMENT);
-        System.out.println(SECTION_MANAGEMENT);
-        System.out.println(DISPLAY_ROUTE);
-        System.out.println(EXIT);
+    }
+
+    public void printStationMenu() {
+        System.out.println(STATION_MENU);
     }
 }
