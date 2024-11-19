53e92251af92db3b6caf313d2dd2e187300723dc
kgy1008 <gayeon1008@g.skku.edu>
2024-11-19 15:40:48 +0900

    feat(Station): 지하철 역 생성 제약 조건 설정

---
+++ src/main/java/subway/common/ErrorMessage.java
@@ -0,0 +1,19 @@
+package subway.common;
+
+public enum ErrorMessage {
+    INVALID_STATION_NAME_LENGTH("역의 이름은 2글자 이상이여야 합니다."),
+    CONFLICT_STATION("이미 해당 역이 존재합니다."),
+    INVALID_STATION_NAME_FORMAT("역 이름의 포맷이 잘못되었습니다. - XX역 형식으로 한글을 입력해주세요.");
+
+
+    private static final String HEADER = "[ERROR] ";
+    private final String message;
+
+    ErrorMessage(final String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return HEADER + message;
+    }
+}
---
+++ src/main/java/subway/domain/station/Station.java
@@ -0,0 +1,51 @@
+package subway.domain.station;
+
+import subway.common.ErrorMessage;
+
+public class Station {
+
+    private static final int MIN_LENGTH = 2;
+    public static final char STATION_LAST_NAME = '역';
+    private static final String HANGUL_REGEX = "^[가-힣]+$";
+
+    private final String name;
+
+    public Station(final String name) {
+        validate(name);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void validate(final String name) {
+        validateFormat(name);
+        validateName(name);
+        validateDuplicate(name);
+    }
+
+    private void validateFormat(final String name) {
+        if (name.matches(HANGUL_REGEX) && findLastName(name) == STATION_LAST_NAME) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_STATION_NAME_FORMAT.getMessage());
+    }
+
+    private char findLastName(final String name) {
+        return name.charAt(name.length() - 1);
+    }
+
+    private void validateName(final String name) {
+        if (name.length() >= MIN_LENGTH) {
+            return;
+        }
+        throw new IllegalArgumentException(ErrorMessage.INVALID_STATION_NAME_LENGTH.getMessage());
+    }
+
+    private void validateDuplicate(final String name) {
+        if (StationRepository.existsByname(name)) {
+            throw new IllegalArgumentException(ErrorMessage.CONFLICT_STATION.getMessage());
+        }
+    }
+}
