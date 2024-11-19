995b1e32a3497ce80fad4626b77297bd1a694cd8
kgy1008 <gayeon1008@g.skku.edu>
2024-11-19 20:42:20 +0900

    fix(Initializer): 초기화 시, 불변 리스트를 사용하지 않음으로써 변경 가능하도록 수정

--- src/main/java/subway/config/Initializer.java
+++ src/main/java/subway/config/Initializer.java
@@ -1,5 +1,6 @@
 package subway.config;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import subway.domain.line.Line;
@@ -20,16 +21,21 @@ public class Initializer {
             new Station("양재시민의숲역"),
             new Station("매봉역")
     );
+
     private static final List<Line> INITIAL_LINE = List.of(
             new Line("2호선"),
             new Line("3호선"),
             new Line("신분당선")
     );
+    
     private static final Set<Section> INITIAL_SECTION = Set.of(
-            new Section(new Line("2호선"), List.of(new Station("교대역"), new Station("강남역"), new Station("역삼역"))),
+            new Section(new Line("2호선"),
+                    new ArrayList<>(List.of(new Station("교대역"), new Station("강남역"), new Station("역삼역")))),
             new Section(new Line("3호선"),
-                    List.of(new Station("교대역"), new Station("남부터미널역"), new Station("양재역"), new Station("매봉역"))),
-            new Section(new Line("신분당선"), List.of(new Station("강남역"), new Station("양재역"), new Station("양재시민의숲역")))
+                    new ArrayList<>(List.of(new Station("교대역"), new Station("남부터미널역"), new Station("양재역"),
+                            new Station("매봉역")))),
+            new Section(new Line("신분당선"),
+                    new ArrayList<>(List.of(new Station("강남역"), new Station("양재역"), new Station("양재시민의숲역"))))
     );
 
     public static void init() {
